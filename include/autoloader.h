a56a4d90b78763aa0a90f377fa2e21d7d3654534
najdek <4890607+najdek@users.noreply.github.com>
2026-06-12 20:11:04 +0200

    bump version to 0.1.1

--- include/autoloader.h
+++ include/autoloader.h
@@ -4,7 +4,7 @@
  * ps5-autoloader — version & config
  * ----------------------------------------------------------------------- */
 
-#define AUTOLOADER_VERSION "0.1.0"
+#define AUTOLOADER_VERSION "0.1.1"
 
 /* Port that elfldr (socksrv) listens on for incoming ELF payloads */
 #define ELFLDR_PORT 9021
